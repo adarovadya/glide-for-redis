valkey-io/valkey-glide

change otel config to a static global object
Signed-off-by: Adar Ovadia <adarov@amazon.com>

glide-core/src/lib.rs
@@ -15,5 +15,7 @@ pub mod scripts_container;
 pub use client::ConnectionRequest;
 pub mod cluster_scan_container;
 pub mod request_type;
-pub use telemetrylib::Telemetry;
-pub use telemetrylib::{GlideOpenTelemetry, GlideSpan};
+pub use telemetrylib::{
+    GlideOpenTelemetry, GlideOpenTelemetryConfigBuilder, GlideOpenTelemetryTraceExporter,
+    GlideSpan, Telemetry, DEFAULT_FLUSH_SIGNAL_INTERVAL_MS, DEFAULT_TRACE_REQUESTS_PRESCENTAGE,
+};

adarovadya/glide-for-redis

glide-core/src/lib.rs
@@ -15,5 +15,7 @@ pub mod scripts_container;
 pub use client::ConnectionRequest;
 pub mod cluster_scan_container;
 pub mod request_type;
-pub use telemetrylib::Telemetry;
-pub use telemetrylib::{GlideOpenTelemetry, GlideSpan};
+pub use telemetrylib::{
+    GlideOpenTelemetry, GlideOpenTelemetryConfigBuilder, GlideOpenTelemetrySignalsExporter,
+    GlideSpan, Telemetry, DEFAULT_FLUSH_SIGNAL_INTERVAL_MS, DEFAULT_TRACE_REQUESTS_PRESCENTAGE,
+};